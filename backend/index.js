137c7092f2db34c5e6160472de42182d2d9761b2
David Petrosian <david.petrosian@stud.vgtu.lt>
2019-04-23 21:14:50 +0200

    First commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-require('./app');
+$.getJSON("https://api.chucknorris.io/jokes/random", function(data){
+console.log(data);
+
+    var icon_url = "https://assets.chucknorris.host/img/avatar/chuck-norris.png";
+    console.log(icon_url);
+
+}
+
+);
